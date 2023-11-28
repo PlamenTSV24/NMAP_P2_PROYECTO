151f9150ce34aedb9380ff53280e02736eed1ce7
PlamenTSV24 <131437888+PlamenTSV24@users.noreply.github.com>
2023-11-28 12:15:51 +0100

    funciones_db.py

--- funciones_db.py
+++ funciones_db.py
@@ -1,4 +1,8 @@
 import pymysql
+import hashlib
+
+def obtener_hash(password):
+    return hashlib.sha256(password.encode()).hexdigest()
 
 def obtener_ip_por_nombre(nombre_maquina):
     conexion = obtener_conexion()
@@ -21,10 +25,34 @@ def registrar_usuario(usuario, password, tokens):
     conexion = obtener_conexion()
     cursor = conexion.cursor()
 
+    password_hasheada = obtener_hash(password)
+    
     query = "INSERT INTO usuarios (usuario, pass, tokens) VALUES (%s, %s, %s)"
 
-    cursor.execute(query, (usuario, password, tokens))
+    cursor.execute(query, (usuario, password_hasheada, tokens))
     conexion.commit()
+
+    cursor.close()
+    conexion.close()
+
+def verificar_credenciales(usuario, password):
+    conexion = obtener_conexion()
+    cursor = conexion.cursor()
+
+    query = "SELECT pass FROM usuarios WHERE usuario = %s"
+    cursor.execute(query, (usuario,))
+    resultado = cursor.fetchone()
+
+    if resultado:
+        password_hasheada_bd = resultado[0]
+        password_hasheada_ingresada = obtener_hash(password) 
+
+        if password_hasheada_bd == password_hasheada_ingresada:
+            return True
+        else:
+            return False
+    else:
+        return False
 
     cursor.close()
     conexion.close()
