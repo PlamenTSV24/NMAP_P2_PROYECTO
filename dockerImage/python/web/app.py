7a7ca48dd77d094a63db77ab24bd25e7153945cb
BiCH0 <ivanmartinezalcoceba@gmail.com>
2024-04-16 09:43:44 +0200

    Securizado 1/?

--- dockerImage/python/web/app.py
+++ dockerImage/python/web/app.py
@@ -1,17 +1,19 @@
 import os
 from flask import Flask
-from variables_database import cargarvariables
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
-app.config.from_pyfile('settings.py')
-cargarvariables()
-  
-import funciones_main as funciones_main
+app.config['production'] = True
+app.config.update(
+    SECRET_KEY=os.environ.get("SECRET_KEY")
+)
+# csrf = CSRFProtect(app)
 
+import funciones_main as funciones_main
 import database
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT'))
     host = os.environ.get('HOST')
-    app.run(host=host, port=port)
+    app.run(host=host, port=port) #Añadir , ssl_context='adhoc' para https
--- dockerImage/python/web/funciones_main.py
+++ dockerImage/python/web/funciones_main.py
@@ -3,7 +3,12 @@ from __main__ import app
 from flask import render_template, session,request
 from funciones_db import verificar_credenciales,verificar_usuario,registrar_usuario,restar_token,cambiar_pass,borrar_usuario
 from enymeep import scanTarget
+import datetime
+from werkzeug.http import http_date
+import bleach
+import logging
 
+logging.basicConfig(level="INFO")
 
 @app.route("/",methods=['GET'])
 def raiz():
@@ -15,8 +20,7 @@ def raiz():
 def login():
     requestLogin = request.get_json()
     #try:
-    username_bd = verificar_credenciales(requestLogin['username'],requestLogin['password'])
-    print(username_bd)
+    username_bd = verificar_credenciales(bleach.clean(requestLogin['username']),bleach.clean(requestLogin['password']))
     if username_bd is False:
         res = {"status": 406 ,"mensaje":"Invalid username or password" }
     
@@ -26,6 +30,7 @@ def login():
         # return render_template("control.html")
         res = {"status": 301 ,"mensaje":"Login correct", "target":"/dashboard"}
 
+    logging.info(f"Login from user {requestLogin['username']}: {res['mensaje']}")
     # except:
     #     res={"status": 406, "mensaje" : "An error has occurred while loading session"}
 
@@ -33,7 +38,7 @@ def login():
 
 @app.route("/dashboard",methods=['POST'])
 def dashboard():
-    usuario=session.get("usuario") 
+    usuario=session.get("usuario")
     if usuario is None:
         return {"status": 403, "mensaje":"Forbidden, go suck pp"}
     return render_template("control.html",user=usuario,tokens=session.get('tokens'))
@@ -43,24 +48,24 @@ def registro():
     requestRegister = request.get_json()
         
     try:
-        user_exist = verificar_usuario(requestRegister['username'])
+        user_exist = verificar_usuario(bleach.clean(requestRegister['username']))
         if user_exist == True:
             res = {"status": 406 ,"mensaje":"User exists" }
 
         else:
-            registrar_usuario(requestRegister['username'],requestRegister['password'])
+            registrar_usuario(bleach.clean(requestRegister['username']),bleach.clean(requestRegister['password']))
             res = {"status": 200 , "mensaje":"You have been registered successfully"}
 
     except:
         res = {"status": 500 ,"mensaje":"An error has occurred while registering" }
-
+    logging.info(f"New user {requestRegister['username']}: {res['mensaje']}")
     return res
 
 @app.route("/password",methods=['POST'])
 def password():
     requestBody = request.get_json()
     usuario = session.get("usuario")
-    pwd = requestBody["pwd"]
+    pwd = bleach.clean(requestBody["pwd"])
     res = False
     if usuario is not None and pwd != "":
         res = cambiar_pass(usuario,pwd)
@@ -68,6 +73,7 @@ def password():
         res={"status": 200 , "mensaje":"Okay!!"}
     else:
         res={"status": 406 , "mensaje":"Error :("}
+    logging.info(f"User {usuario} tried to change password: {res['mensaje']}")
     return res
 
 @app.route("/delete",methods=['POST'])
@@ -78,6 +84,7 @@ def delete():
         res={"status": 200 , "mensaje":"Deleted"}
     else:
         res={"status": 406 , "mensaje":"Error >:("}
+    logging.info(f"User {usuario} tried to change password: {res['mensaje']}")
     return res
 
 
@@ -85,14 +92,46 @@ def delete():
 
 @app.route("/enymeep",methods=['GET','POST'])
 def enymeep():
+    usuario = session.get("usuario")
     requestBody = request.get_json()
     if requestBody['free'] != "True":
-        restar_token(session.get("usuario"))
-    res = scanTarget(requestBody['address'], requestBody['ports'])
+        restar_token(usuario)
+    res = scanTarget(bleach.clean(requestBody['address']), bleach.clean(requestBody['ports']))
+    logging.info(f"User {usuario} started scan: {requestBody['address']}:{requestBody['ports']}")
     return res
 
 @app.route("/calculariva", methods=["GET"])
 def calculariva(valor=0):
     if valor == 0:
-        valor = request.args.get('valor', default = 1, type = int)
+        valor = bleach.clean(request.args.get('valor', default = 1, type = int))
     return {"status": 200 , "resultado":valor*0.21}
+
+def prepare_response_extra_headers():
+    response_extra_headers = {
+        # always
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0',
+        'Last-Modified': http_date(datetime.datetime.now()),
+    }
+    response_security_headers = {
+        # X-Frame-Options: page can only be shown in an iframe of the same site
+        'X-Frame-Options': 'SAMEORIGIN',
+        # ensure all app communication is sent over HTTPS
+        'Strict-Transport-Security': 'max-age=63072000; includeSubdomains',
+        # instructs the browser not to override the response content type
+        'X-Content-Type-Options': 'nosniff',
+        # enable browser cross-site scripting (XSS) filter
+        'X-XSS-Protection': '1; mode=block',
+    }
+    response_extra_headers.update(response_security_headers)
+
+    return response_extra_headers
+
+response_extra_headers = prepare_response_extra_headers()
+
+
+@app.after_request
+def after_request(response):
+    response.headers.extend(response_extra_headers)
+    return response
